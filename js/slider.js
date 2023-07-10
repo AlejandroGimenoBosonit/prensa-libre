0b58c2bb62c02dfc60ac3d0c2ed70757ffc9221d
AlejandroGimenoBosonit <alejandro.gimeno@bosonit.com>
2023-07-10 10:17:15 +0200

    animations done

--- js/slider.js
+++ js/slider.js
@@ -17,16 +17,33 @@ const clicked = (id) => {
     document.getElementById("banner-1-title").className = "animate-title";
 
 
+
+    // banner-2
+    document.getElementById('banner-2-title').className = 'animate-banner-2-title';
+    const elements = document.querySelectorAll('#img-grid');
+    elements.forEach(el => {
+      el.className = 'grid-container-inverted ';
+    });
+
     // banner-3
     document.getElementById("bote2-img").className = 'animate-bote2-2'
     document.getElementById("banner-3-title").className = 'animate-banner-3-title-inverted';
 
-  } else if (id === "button_b") {
+  } else if (id === "button_b" 
+  && document.getElementById('banner-2-title').className == "animate-banner-2-title"
+  ) {
+    // banner-2
+    document.getElementById('banner-2-title').className = 'animate-banner-2-title-inverted';
+    const elements = document.querySelectorAll('#img-grid');
+    elements.forEach(el => {
+      el.className = 'grid-container ';
+    });
+    
+    
+    
     // banner-1
     document.getElementById("bote-img").className = "animate-bote";
     document.getElementById("banner-1-title").className = "animate-title-2";
-  
-    
     // banner-3
     document.getElementById("bote2-img").className = 'animate-bote2-2'
     document.getElementById("banner-3-title").className = 'animate-banner-3-title-inverted';
@@ -40,18 +57,16 @@ const clicked = (id) => {
     document.getElementById("bote2-img").className = 'animate-bote2';
     document.getElementById("banner-3-title").className = 'animate-banner-3-title';
 
-
-
-
-
-
-
-
-
-
     // banner-1
     document.getElementById("bote-img").className = "animate-bote";
     document.getElementById("banner-1-title").className = "animate-title-2";
+    // banner-2
+    document.getElementById('banner-2-title').className = 'animate-banner-2-title';
+    const elements = document.querySelectorAll('#img-grid');
+    elements.forEach(el => {
+      el.className = 'grid-container-inverted ';
+    });
+    
     
   }
 };
