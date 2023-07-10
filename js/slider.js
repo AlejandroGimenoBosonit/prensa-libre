a83d3b00ff353ad7ce507aaf90568d6f7573809d
AlejandroGimenoBosonit <alejandro.gimeno@bosonit.com>
2023-07-10 10:36:58 +0200

    fixing scripts

--- js/slider.js
+++ js/slider.js
@@ -10,63 +10,59 @@ const changeEllipsis = (event) => {
 const clicked = (id) => {
   if (
     id === "button_a" &&
-    document.getElementById("bote-img").className == "animate-bote" 
+    document.getElementById("bote-img").className == "animate-bote"
   ) {
     // banner-1
     document.getElementById("bote-img").className = "animate-bote-2";
     document.getElementById("banner-1-title").className = "animate-title";
 
-
-
     // banner-2
-    document.getElementById('banner-2-title').className = 'animate-banner-2-title';
-    const elements = document.querySelectorAll('#img-grid');
-    elements.forEach(el => {
-      el.className = 'grid-container-inverted ';
-    });
+    document.getElementById("banner-2-title").className =
+      "animate-banner-2-title";
+    selectMultiElements("#img-grid", "grid-container-inverted");
 
     // banner-3
-    document.getElementById("bote2-img").className = 'animate-bote2-2'
-    document.getElementById("banner-3-title").className = 'animate-banner-3-title-inverted';
-
-  } else if (id === "button_b" 
-  && document.getElementById('banner-2-title').className == "animate-banner-2-title"
+    document.getElementById("bote2-img").className = "animate-bote2-2";
+    document.getElementById("banner-3-title").className =
+      "animate-banner-3-title-inverted";
+  } else if (
+    id === "button_b" &&
+    document.getElementById("banner-2-title").className ==
+      "animate-banner-2-title"
   ) {
     // banner-2
-    document.getElementById('banner-2-title').className = 'animate-banner-2-title-inverted';
-    const elements = document.querySelectorAll('#img-grid');
-    elements.forEach(el => {
-      el.className = 'grid-container ';
-    });
-    
-    
-    
+    document.getElementById("banner-2-title").className =
+      "animate-banner-2-title-inverted";
+    selectMultiElements("#img-grid", "grid-container");
+
     // banner-1
     document.getElementById("bote-img").className = "animate-bote";
     document.getElementById("banner-1-title").className = "animate-title-2";
     // banner-3
-    document.getElementById("bote2-img").className = 'animate-bote2-2'
-    document.getElementById("banner-3-title").className = 'animate-banner-3-title-inverted';
-
-
-
-  } else if(id === "button_c" &&
-    document.getElementById("bote2-img").className == 'animate-bote2-2'
-  ){
+    document.getElementById("bote2-img").className = "animate-bote2-2";
+    document.getElementById("banner-3-title").className =
+      "animate-banner-3-title-inverted";
+  } else if (
+    id === "button_c" &&
+    document.getElementById("bote2-img").className == "animate-bote2-2"
+  ) {
     // banner-3
-    document.getElementById("bote2-img").className = 'animate-bote2';
-    document.getElementById("banner-3-title").className = 'animate-banner-3-title';
-
+    document.getElementById("bote2-img").className = "animate-bote2";
+    document.getElementById("banner-3-title").className =
+      "animate-banner-3-title";
     // banner-1
     document.getElementById("bote-img").className = "animate-bote";
     document.getElementById("banner-1-title").className = "animate-title-2";
     // banner-2
-    document.getElementById('banner-2-title').className = 'animate-banner-2-title';
-    const elements = document.querySelectorAll('#img-grid');
-    elements.forEach(el => {
-      el.className = 'grid-container-inverted ';
-    });
-    
-    
+    document.getElementById("banner-2-title").className =
+      "animate-banner-2-title";
+    selectMultiElements("#img-grid", "grid-container-inverted");
   }
+};
+
+const selectMultiElements = (id, newClass) => {
+  const elements = document.querySelectorAll(id);
+  elements.forEach((el) => {
+    el.className = newClass;
+  });
 };
